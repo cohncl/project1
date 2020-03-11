7702e5d9a7ba28f514866671dca7ca24968ff464
charles-cohn <cohncl@gmatc.matc.edu>
2020-03-11 02:14:36 -0500

    Final assignment submission

---
+++ contentful-blog-01/src/pages/index.js
@@ -0,0 +1,89 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+import Img from "gatsby-image"
+import styled from "styled-components"
+
+import Bio from "../components/bio"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { rhythm } from "../utils/typography"
+
+const Post = styled.div`
+  display: flex;
+`
+
+const PostImage = styled.div`
+  flex: 25%;
+  margin-right: 1rem;
+`
+
+const PostText = styled.div`
+  flex: 75%;
+`
+
+const BlogIndex = ({ data, location }) => {  
+  const siteTitle = data.siteTitle
+  const posts = data.allContentfulPost.edges
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO title="All posts" />
+      <Bio />
+      {posts.map(({ node }) => {
+        const title = node.title || node.slug
+        return (
+          <Post key={node.slug}>
+            <PostImage>
+              <Img fluid={node.image.fluid} />
+            </PostImage>
+            
+              <PostText>
+                <h3
+                  style={{
+                    marginTop: 0,
+                    marginBottom: rhythm(1 / 4),
+                  }}
+                >
+                  <Link style={{ boxShadow: `none` }} to={node.slug}>
+                    {title}
+                  </Link>
+                </h3>
+              
+            
+            
+              {node.subtitle}       
+            </PostText>  
+            
+          </Post>
+        )
+      })}
+    </Layout>
+  )
+}
+
+export default BlogIndex
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allContentfulPost {
+      edges {
+        node {
+          title
+          subtitle
+          image {
+            fluid{
+              ...GatsbyContentfulFluid
+            }
+          }
+          author
+          slug
+        }
+      }
+    }
+  }
+`
---
+++ contentful-blog-01/src/templates/blog-post-contentful.js
@@ -0,0 +1,100 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+import Bio from "../components/bio"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { rhythm } from "../utils/typography"
+
+const BlogPostContentfulTemplate = ({ data, pageContext, location }) => {
+  const post = this.props.data.contentfulPost
+  const siteTitle = this.props.data.site.siteMetadata.title
+  const { previous, next } = pageContext
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO
+        title={post.title}
+        description={post.subtitle}
+      />
+      <Img fluid={post.image.fluid} />
+      <article>
+        <header>
+          <h1
+            style={{
+              marginTop: rhythm(1),
+              marginBottom: 0,
+            }}
+          >
+            {post.title}
+          </h1>
+          
+        </header>
+        <section dangerouslySetInnerHTML={{ __html: post.content.childContentfulRichText.html }} />
+        <hr
+          style={{
+            marginBottom: rhythm(1),
+          }}
+        />
+        <footer>
+          <Bio />
+        </footer>
+      </article>
+
+      <nav>
+        <ul
+          style={{
+            display: `flex`,
+            flexWrap: `wrap`,
+            justifyContent: `space-between`,
+            listStyle: `none`,
+            padding: 0,
+          }}
+        >
+          <li>
+            {previous && (
+              <Link to={previous.slug} rel="prev">
+                ← {previous.title}
+              </Link>
+            )}
+          </li>
+          <li>
+            {next && (
+              <Link to={next.slug} rel="next">
+                {next.title} →
+              </Link>
+            )}
+          </li>
+        </ul>
+      </nav>
+    </Layout>
+  )
+}
+
+export default BlogPostContentfulTemplate
+
+export const pageQuery = graphql`
+  query ContentfulBlogPostBySlug($slug: String!) {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    contentfulPost( slug: { eq: $slug }) {
+      title
+      subtitle
+      author
+      image {
+        fluid{
+          ...GatsbyContentfulFluid
+        }
+      }
+      content {
+        childContentfulRichText {
+          html
+        }
+      }
+    }
+  }
+`
